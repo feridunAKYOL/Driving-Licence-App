3a2c5b94a78e3cdc96ba1f75e539f599976dd31d
ayseakyol <ayseakyol.22309@gmail.com>
2020-07-24 14:54:32 +0200

    front end test page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
-import React, { Component } from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
-import withAuth from './withAuth';
-import Home from './Home';
-import Secret from './Secret';
-import Login from './Login';
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import "./App.css";
+//import withAuth from "./withAuth";
+import Home from "./Home";
+//import Secret from "./Secret";
+//import Login from "./Login";
+import FreeTest from "./components/FreeTest/FreeTest";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <ul>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/freetest" component={FreeTest} />
+          <Redirect to="/" />
+        </Switch>
+        {/* <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/secret">Secret</Link></li>
           <li><Link to="/login">Login</Link></li>
         </ul>
-
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/secret" component={withAuth(Secret)} />
           <Route path="/login" component={Login} />
-        </Switch>
-      </div>
+        </Switch> */}
+      </Router>
     );
   }
 }

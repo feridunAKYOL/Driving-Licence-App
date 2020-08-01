0b0185b879cf31beb1e3e4483126c19936cb811f
feridun Akyol <feridunakyol34@gmail.com>
2020-08-01 13:43:46 +0200

    result page is done

--- client/src/components/resultsPage/Results.jsx
+++ client/src/components/resultsPage/Results.jsx
@@ -18,18 +18,15 @@ const Result = () => {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ "userAnswer": userAnswer })
     };
-    const fetchResult = () => {
+    const fetchResult =  () => {
       return fetch("/api/tests/result/'equipment'", request_result)
         .then((response) => response.json())
         .then((data) => data);
     };
-    console.log(fetchResult());
+    console.log(fetchResult() , 1);
     fetchResult().then((el) => {
       setResults(el);
-      console.log(results);
-      console.log(userAnswer);
     });
-
 
       // Fetch file names
       const fetchFileNames = () => {
@@ -43,7 +40,6 @@ const Result = () => {
       });
 
   }, []);
-
   return (
     <>
       <ResultsNavbar />
@@ -51,18 +47,26 @@ const Result = () => {
         <Row className="picture-part">
           {fileNames.filter((obj) => 
            obj.testName === 'test-1'
-          ).map((obj, id) => (
-            <Col key={id} xs={5} md={3} lg={2} className="m-4 p-1" style={{ border: "groove 3px red", }} >
-              <Row className="justify-content-center">{id + 1}</Row>
+          )
+          .sort((a, b) => Number(a.situationNumber) - Number(b.situationNumber))
+          .map((obj, id) => (
+            results.filter(el => Number(el.situationNumber) === Number(obj.situationNumber))
+            .map(a => (
+            <Col key={id} xs={5} md={3} lg={2} className="m-4 p-1" 
+               style = {a.result ? { border: "groove 3px green" } : { border: "groove 3px red" } }
+            >
+              <Row className="justify-content-center">{obj.situationNumber}</Row>
               <Row className="justify-content-center">
                 <Link to="/oneResultPage">
                   <Image key={id} src={obj.fileRelativePath} height="180px" width="180px" className="p-2" />
                   {" "}
                 </Link>
               </Row>
-              <Row className="justify-content-center">False</Row>
+          <Row className="justify-content-center"> 
+                  <p> {a.result ? "True" : "False"} </p> 
+          </Row>
             </Col>
-          ))}
+          ))))}
         </Row>
       </Jumbotron>
     </>

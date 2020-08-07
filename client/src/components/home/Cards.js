f50268343a70b1484b21c54b20b72b6a59c1ac2f
feridun Akyol <feridunakyol34@gmail.com>
2020-08-07 14:29:44 +0200

    fix cards after merge

--- client/src/components/home/Cards.js
+++ client/src/components/home/Cards.js
@@ -1,16 +1,7 @@
 import React, { Component } from "react";
 import "./Cards.css";
-import {
-  Row,
-  Col,
-  Card,
-  Button,
-  CardDeck,
-  Container,
-  Image,
-  Jumbotron,
-} from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Row, Col, Card, Button, CardDeck, Container, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 class Cards extends Component {
   constructor() {
@@ -35,75 +26,81 @@ class Cards extends Component {
 
   render() {
     return (
-      <div id="cards-part">
-        <h2 className="text-center how" style={{ fontSize: "2.2rem" }}>
-          How We Work
-        </h2>
-        <Row className="practice-text">
-          <Col className="m-3" xs={11} md={3}>
-            <Card>
-              <Card.Img variant="top" src="/assets/onlineTutorials.jpg" />
-              <Card.Body>
-                <Card.Title>Online Tutorials</Card.Title>
-                <Card.Text>
-                  With the online tutorials you learn a lot of thing different
-                  things. You can imagine you as a driver while watching the
-                  videos.
-                </Card.Text>
-                <Link to="/tutorials">
-                  <Button variant="secondary" className="card-button" size="lg">
-                    Videos
-                  </Button>
-                </Link>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="m-3" xs={11} md={3}>
-            <Card>
-              <Card.Img variant="top" src="/assets/practice.jpg" />
-              <Card.Body>
-                <Card.Title>Buy Access</Card.Title>
-                <Card.Text>
-                  When you registered to our page you can find more tests. More
-                  practice makes you perfect so you can easily pass the police
-                  test at first try.
-                </Card.Text>
-                <Link to={this.state.path}>
-                  <Button variant="secondary" size="lg">
-                    {this.state.buttonName}
-                  </Button>
-                </Link>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="justify-content-around m-3" xs={11} md={3}>
-            <Card>
-              <Card.Img variant="top" src="/assets/drivingTestTips.jpg" />
-              <Card.Body>
-                <Card.Title>Driving Test Tips</Card.Title>
-                <Card.Text>
-                  The pages have been prepared to give you some of the most
-                  important rules to remember in your theory test. Read and
-                  learn them for your achievement.
-                </Card.Text>
-                <Link to="/drivingTips">
-                  <Button variant="secondary" size="lg">
-                    Tips
-                  </Button>
-                </Link>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-        <Jumbotron fluid className="my-5">
-          <h1>Our Coaching:</h1>
-          <h2 className="text-center coaching">
-            Skid-Control + Anti-Stress Driving
-          </h2>
-        </Jumbotron>
-      </div>
-    );
-  }
-}
+      <Container id="cards-part" className="cards-part">
+			<Row>
+				<Col>
+					<h2 className="text-center how" style={{ fontSize: '2.2rem' }}>
+						How We Work
+					</h2>
+				</Col>
+			</Row>
+
+			<CardDeck>
+				<Card className="text-center">
+					<Card.Img variant="top" src="/assets/onlineTutorials.jpg" />
+					<Card.Body>
+						<Card.Title className="head-card">Online Tutorials</Card.Title>
+						<Card.Text>
+							With the online tutorials you learn a lot of thing different things. You can imagine you as
+							a driver while watching the videos. They teach new rules to you.
+						</Card.Text>
+					</Card.Body>
+						<Card.Link className ="mb-2" href="/tutorials">
+							<Button variant="secondary" size="lg" className="card-button">
+								Videos
+							</Button>
+						</Card.Link>
+				</Card>
+
+				<Card className="text-center ">
+					<Card.Img variant="top" src="/assets/practice.jpg" />
+					<Card.Body>
+						<Card.Title className="head-card">Buy Access</Card.Title>
+						<Card.Text>
+							When you registered to our page you can find more tests. More practice makes you perfect so
+							you can easily pass the police test at first try.{' '}
+						</Card.Text>
+					</Card.Body>
+            <Card.Link className="mb-2" href="/drivingTips">
+							<Button variant="secondary" size="lg" className="card-button ">
+								Sign Up
+							</Button>
+						</Card.Link>
+				</Card>
+
+				<Card className="text-center">
+					<Card.Img variant="top" src="/assets/drivingTestTips.jpg" />
+					<Card.Body>
+						<Card.Title className="head-card">Driving Test Tips</Card.Title>
+						<Card.Text>
+							The pages have been prepared to give you some of the most important rules to remember in
+							your theory test. Read and learn them for your achievement.
+						</Card.Text>
+					</Card.Body>
+            <Card.Link className="mb-2" href="drivingTips">
+							<Button variant="secondary" size="lg" className="card-button">
+								Tips
+							</Button>
+						</Card.Link>
+				</Card>
+			</CardDeck>
+
+			<Row>
+				<Col xs={{ span: 10, offset: 1 }} md={{ span: 4, offset: 4 }} className="mt-5 mb-2">
+          <a href="#practice-part">
+            <Image
+              src="/assets/bottom.png"
+              className="go d-none d-xl-block"
+              roundedCircle
+            />
+          </a>
+
+				</Col>
+			</Row>
+
+		</Container>
+  );
+    }
+};
 
 export default Cards;
